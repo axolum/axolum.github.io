5eb0772837a67acc132a33caf98c1e4097df8bcb
Axolum <axolum7cc@gmail.com>
2022-07-23 17:52:30 +0400

    exo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,4 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.font = "30px verdana";
-ctx.strokeText("AXOLUM", 0, 50);
+ctx.strokeText("AXO OR EXO", 0, 50);
